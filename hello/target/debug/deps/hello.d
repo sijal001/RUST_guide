d13ce01650599f83cc3a920a7eb865e5f667fb7c
sijal001 <sijal.joshi@gmail.com>
2023-05-03 14:35:31 +0200

    Rust guide learn by building application

--- hello/target/debug/deps/hello.d
+++
@@ -1,5 +0,0 @@
-C:\Users\GEN\Desktop\projects\RUST_guide\hello\target\debug\deps\hello.exe: src\main.rs
-
-C:\Users\GEN\Desktop\projects\RUST_guide\hello\target\debug\deps\hello.d: src\main.rs
-
-src\main.rs:
--- hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-C:\Users\GEN\Desktop\projects\RUST_guide\hello\target\debug\hello.exe: C:\Users\GEN\Desktop\projects\RUST_guide\hello\src\main.rs
